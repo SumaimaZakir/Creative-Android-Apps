5e4b2fe810d0393c0a02f08ea0ce6b0868592b5f
Sumaima Zakir <77928493+SumaimaZakir@users.noreply.github.com>
2025-04-14 20:48:02 +0400

    commit

---
+++ TipoftheDayCalculator/app/src/main/java/com/sumaima/tipofthedaycalculator/AboutActivity.kt
@@ -0,0 +1,17 @@
+package com.sumaima.tipofthedaycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        val closeBtn: Button = findViewById(R.id.closeBtn)
+        closeBtn.setOnClickListener {
+            finish()
+        }
+    }
+}
